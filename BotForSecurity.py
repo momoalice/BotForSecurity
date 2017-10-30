c51ed4e28e35528dde08413a0a963a79659082e8
Alice Chen <mc3917@columbia.edu>
2017-10-29 20:16:16 -0400

    Default message

--- BotForSecurity.py
+++ BotForSecurity.py
@@ -1,11 +1,14 @@
 import os
 import time
+import json
 from slackclient import SlackClient
 
-BOT_TOKEN = 'xoxb-263720460930-cf80OYJWFdckZl55dPFhtaeW'
+BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 BOT_ID = 'U7RM6DJTC'
 BOT_NAME = 'alicechen'
 SOCKET_DELAY = 1
+with open('nvdcve-1.0-recent.json') as json_data:
+	d = json.load(json_data)
 
 AT_BOT = "<@U7RM6DJTC:>"
 COMMAND1 = "1"
@@ -37,7 +40,8 @@ def is_for_me(event):
             return True
 def handle_message(message, user, channel):
     # TODO Implement later
-    post_message(message='Hello', channel=channel)
+    default_msg = "Hi, this is BotForSecurity. Here are a couple of command:\n1. Counts: return how many data entries do we have\n2. Recent: Get the description for the most recent insecurity\n 3. Latest: Get the time for the most recent insecure activity\n 4. Types: Get all the types of insecure activities"
+    post_message(message=default_msg, channel=channel)
 
 def post_message(message, channel):
     slack_client.api_call('chat.postMessage', channel=channel,
@@ -45,6 +49,7 @@ def post_message(message, channel):
 def run():
     if slack_client.rtm_connect():
         print('is ON...')
+
         while True:
             event_list = slack_client.rtm_read()
             if len(event_list) > 0:
