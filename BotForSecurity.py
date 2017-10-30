99ee65ae53a264eff3739395ee5db6d9e14af3fa
Alice Chen <mc3917@columbia.edu>
2017-10-29 20:37:04 -0400

    Skeleton code for all commands. Implement the first command Counts

--- BotForSecurity.py
+++ BotForSecurity.py
@@ -10,14 +10,11 @@ SOCKET_DELAY = 1
 with open('nvdcve-1.0-recent.json') as json_data:
 	d = json.load(json_data)
 
-AT_BOT = "<@U7RM6DJTC:>"
-COMMAND1 = "1"
-COMMAND2 = "2"
-COMMAND3 = "3"
-COMMAND4 = "4"
-COMMAND5 = "5"
+default_msg = "Hi, this is BotForSecurity. Here are a couple of command:\n1. Counts: return how many data entries do we have\n \t subcommand Access <NUM>\n2. Recent: Get the description for the most recent insecurity\n 3. Latest: Get the time for the most recent insecure activity\n 4. Types: Get all the types of insecure activities"
 
 slack_client = SlackClient(BOT_TOKEN)
+
+
 
 def get_mention(user):
     return '<@{user}>'.format(user=user)
@@ -40,8 +37,23 @@ def is_for_me(event):
             return True
 def handle_message(message, user, channel):
     # TODO Implement later
-    default_msg = "Hi, this is BotForSecurity. Here are a couple of command:\n1. Counts: return how many data entries do we have\n2. Recent: Get the description for the most recent insecurity\n 3. Latest: Get the time for the most recent insecure activity\n 4. Types: Get all the types of insecure activities"
-    post_message(message=default_msg, channel=channel)
+    if message == 'Counts':
+    	response = "There are %s vulnarable data entries in our current database.\n If you want to access some random ones to see what kind of info we get, enter Access <NUM_You_Want>" % d["CVE_data_numberOfCVEs"]
+    	post_message(message=response, channel=channel)
+    elif message == 'Recent':
+    	response = 'To be implemented'
+    	post_message(message=response, channel=channel)
+    elif message == 'Latest':
+    	response = 'To be implemented'
+    	post_message(message=response, channel=channel)
+    elif message == 'Types':
+    	response == 'To be implemented'
+    	post_message(message=response, channel=channel)
+    else:
+    	post_message(message=default_msg, channel=channel)
+
+    
+    # post_message(message=default_msg, channel=channel)
 
 def post_message(message, channel):
     slack_client.api_call('chat.postMessage', channel=channel,
