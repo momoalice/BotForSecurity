af69fcf25af38b17791537dd61c75c2b901cc968
Alice Chen <mc3917@columbia.edu>
2017-11-02 20:20:06 -0400

    Fix import issue for test

--- tests/test_response_control.py
+++ tests/test_response_control.py
@@ -1,5 +1,5 @@
 import unittest
-from BotForSecurity import response_control
+from BotForSecurity.response_control import *
 import os
 
 class TestBot(unittest.TestCase):
@@ -12,27 +12,27 @@ class TestBot(unittest.TestCase):
 
 	def test_counts(self):
 		expected = "There are 689 vulnarable data entries in our current database.\n If you want to access some random ones to see what kind of info we get, enter Sample <NUM_You_Want>"
-		self.assertTrue(BotForSecurity.postCounts(["Counts"],"")[0] == expected and BotForSecurity.postCounts("Counts","")[1].lower() == "counts")
+		self.assertTrue(postCounts(["Counts"],"")[0] == expected and postCounts("Counts","")[1].lower() == "counts")
 
 	def test_specific(self):
 		expected_error_output = "Invalid input. Specific command takes in one argument. Please reenter."
-		self.assertTrue(BotForSecurity.postSpecific(["Specific"], "Search")[0] == expected_error_output and BotForSecurity.postSpecific("Specific", "Search")[1].lower() == "search")
+		self.assertTrue(postSpecific(["Specific"], "Search")[0] == expected_error_output and postSpecific("Specific", "Search")[1].lower() == "search")
 		# ID not fount
 		expected_response = "No CVE with this ID."
-		actual_response = BotForSecurity.postSpecific("Specific 123".split(" "), "Search")[0]
+		actual_response = postSpecific("Specific 123".split(" "), "Search")[0]
 		self.assertEqual(expected_response,actual_response)
 
 	def test_Search(self):
 		expected_error_output = "Invalid input. Search command takes in one argument. Please reenter."
-		self.assertTrue(BotForSecurity.postSearch(["Search"], "")[0] == expected_error_output and BotForSecurity.postSearch(["Search"], "")[1].lower() == "")
+		self.assertTrue(postSearch(["Search"], "")[0] == expected_error_output and postSearch(["Search"], "")[1].lower() == "")
 
 	def test_Sample(self):
 		expected_error_output = "Invalid input. Sample command takes in one argument. Please reenter."
-		self.assertTrue(BotForSecurity.postSample(["Sample"], "Counts")[0] == expected_error_output and BotForSecurity.postSample(["Sample"], "Counts")[1].lower() == "counts")
+		self.assertTrue(postSample(["Sample"], "Counts")[0] == expected_error_output and postSample(["Sample"], "Counts")[1].lower() == "counts")
 
 	def test_Severity(self):
 		expected_error_output = "Invalid input. Severity command takes in one argument. Please reenter."
-		self.assertTrue(BotForSecurity.postSeverity(["Severity"], "Counts")[0] == expected_error_output and BotForSecurity.postSeverity(["Severity"], "Counts")[1].lower() == "counts")
+		self.assertTrue(postSeverity(["Severity"], "Counts")[0] == expected_error_output and postSeverity(["Severity"], "Counts")[1].lower() == "counts")
 
 
 	# def test_handle(self):
